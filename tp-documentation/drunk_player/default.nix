6583995a2cdba3f65c412b595e00d09c850e6555
Julien Dehos <dehos@univ-littoral.fr>
2023-08-23 14:32:44 +0200

    tp-doc

--- tp-documentation/drunk_player/default.nix
+++ tp-documentation/drunk_player/default.nix
@@ -1,15 +1,36 @@
-with import <nixpkgs> {};
+let 
 
-stdenv.mkDerivation {
+  # Change les paramètres du paquet opencv.
+  # Nécessite de recompiler opencv.
+  config = {
+    packageOverrides = pkgz: {
+      opencv = pkgz.opencv.override { 
+        enableGtk2 = false; 
+        enableGtk3 = true; 
+        enableGStreamer = false;
+        enableFfmpeg = true;
+        enableTIFF = false;
+        enableEXR = false;
+        enableEigen = false;
+        enableContrib = false;
+      };
+    };
+  };
+
+  # pkgs = import <nixpkgs> { inherit config; };
+  pkgs = import <nixpkgs> {};
+
+in pkgs.stdenv.mkDerivation {
   name = "drunk_player";
   src = ./.;
 
-  buildInputs = [
+  buildInputs = with pkgs; [
     boost
     cmake
     doxygen
     pkg-config
-    opencv_gtk
+    opencv
   ];
 
 }
+
